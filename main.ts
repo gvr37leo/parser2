e427c8410f7e1e8f7fea528d3f1c255010bfa629
Paul Boon <gvr37.p.l.boon@gmail.com>
2019-02-10 13:25:16 +0100

    p

--- main.ts
+++ main.ts
@@ -2,51 +2,66 @@
 
 function parse(text:string, system:Knot):TreeNode{
     var root:TreeNode = new TreeNode()
-    var systemStack:Knot[] = [system]
+    var systemStack = [{knot:system,targetKnotIndex:0}]
+
     var stringpointer = 0
-    var currentKnot = system
+    // var currentKnot = system
 
+    debugger
+    mainloop:while(stringpointer < text.length){
+        if(systemStack.length == 0){
+            break
+        }
 
-    mainloop:while(stringpointer < text.length && systemStack.length > 0){
-        for(var i = 0; i < currentKnot.knots.length; i++){
-            var targetKnot = currentKnot.knots[i]
-            if(targetKnot.knotType == KnotType.high){
-                //just go in and go back out if no fit
-                //backing out is a problem because is i is reset to 0 and cant go over the high knot
-                systemStack.push(targetKnot.subsystem)
-                currentKnot = targetKnot.subsystem
-                i = -1
-            }else if(targetKnot.knotType == KnotType.entry){
-                // should never be hit since currentknot is teleported here from a high knot
-                console.log('entry knot should not be hit')
+        var current = last(systemStack)
+        
+        for(; current.targetKnotIndex < current.knot.knots.length; current.targetKnotIndex++){
+            var targetknot = current.knot.knots[current.targetKnotIndex]
 
-            }else if(targetKnot.knotType == KnotType.normal){
-                for(var symbol of targetKnot.allowedSymbols){
+            if(targetknot.knotType == KnotType.high){
+                systemStack.push({knot:targetknot.subsystem,targetKnotIndex:0})
+                break
+            }else if(targetknot.knotType == KnotType.normal){
+                var found = false
+                for(var symbol of targetknot.allowedSymbols){
                     if(text.substr(stringpointer,symbol.length) === symbol){
                         stringpointer += symbol.length
-                        currentKnot = targetKnot
-                        i = -1
+                        current.knot = targetknot
+                        current.targetKnotIndex = -1
+                        found = true
                         break
                     }
                 }
-            }else if(targetKnot.knotType == KnotType.exit){
+                if(!found){
+                    systemStack.pop()
+                    var highknotEnteringNode = last(systemStack)
+                    highknotEnteringNode.targetKnotIndex++
+                }
+            }else if(targetknot.knotType == KnotType.exit){
                 systemStack.pop()
                 if(systemStack.length == 0){
-                    console.log('string leftover at end of parsing')
-                    break mainloop
+                    console.log('finished parsing text, finished final stack')
+                    if(stringpointer < text.length){
+                        console.log('still text left')
+                        break mainloop
+                    }
+                    
+                }else{
+                    var highknotEnteringNode = last(systemStack)
+                    highknotEnteringNode.knot = highknotEnteringNode.knot.knots[highknotEnteringNode.targetKnotIndex]
                 }
-                currentKnot = systemStack[systemStack.length - 1].knots[0]
+                break
             }
-            
         }
-
-        console.log('no matching symbols found')
-        console.log(currentKnot)
-        console.log(stringpointer)
-        break
     }
 
+
+
     return root
+}
+
+function last<T>(arr:T[]){
+    return arr[arr.length - 1]
 }
 
 class TreeNode{
@@ -118,7 +133,7 @@ class Knot{
 
 
 
-var text = '((a)((b)))';
+var text = '((a))';
 var bracesHigh = Knot.subsystem(null)
 var braces = Knot.entry()
 braces.connect(new Knot(['('])).or([bracesHigh,new Knot(['a','b']),new Knot([])],new Knot([')'])).end()
